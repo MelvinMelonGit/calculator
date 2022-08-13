ddb080a0a544f75e017ebc608317dad156e02c7c
Lee Yi Cheng <MelvinLee@MacBook-Pro.local>
2022-08-13 20:43:30 +0800

    Minor js changes, added step by step comments on logic

--- script.js
+++ script.js
@@ -17,9 +17,23 @@ function operate(operator, num1, num2) {
     if (operator === "divide") divide(num1, num2);
 }
 
-function displayOutput(e) {
+function displayOutput(e) { //temporary function, may change later
     const target = e.target.textContent;
     calculatorScreen.textContent = target;
 }
 
 calculatorBottom.addEventListener('click', displayOutput);
+
+//happy case
+//for the display
+//0. instatiate total variable
+//1. user clicks on a number button
+//2. display shows number
+//3. user clicks on operator
+//4. variable saves previous number
+//5. user clicks on number
+//6. user clicks on operator / equal
+//7. variable saves new total
+
+//edge cases
+//to be added
